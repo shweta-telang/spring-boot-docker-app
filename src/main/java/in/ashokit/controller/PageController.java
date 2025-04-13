80aea49a39fe7ec7b875ffdb9c00295ca0d2d0dd
shweta-telang <31919420+shweta-telang@users.noreply.github.com>
2025-04-14 03:55:40 +0530

    Update PageController.java

--- src/main/java/in/ashokit/controller/PageController.java
+++ src/main/java/in/ashokit/controller/PageController.java
@@ -1,1 +1,13 @@
+package in.ashokit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login"; // loads login.html from static/
+    }
+}
