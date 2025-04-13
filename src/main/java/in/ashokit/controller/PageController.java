5a2d33717ab5f5fdfe2045417871acb92ab21d7c
shweta-telang <31919420+shweta-telang@users.noreply.github.com>
2025-04-14 04:29:51 +0530

    Update PageController.java

--- src/main/java/in/ashokit/controller/PageController.java
+++ src/main/java/in/ashokit/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/login")
-    public String loginPage() {
-        return "login";  // refers to templates/login.html
+    public String showLoginPage() {
+        return "login"; // This maps to login.html in resources/templates
     }
 }
