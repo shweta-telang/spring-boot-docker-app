9bbd6247f59e59c959796ae9a45f3080d0ae9e5a
shweta-telang <31919420+shweta-telang@users.noreply.github.com>
2025-04-14 04:51:51 +0530

    Update SbRestAppApplication.java

--- src/main/java/in/ashokit/SbRestAppApplication.java
+++ src/main/java/in/ashokit/SbRestAppApplication.java
@@ -2,14 +2,12 @@ package in.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("in.ashokit")  // 🔥 Add this line!
 public class SbRestAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SbRestAppApplication.class, args);
-		int i  = 10;
-		int j = 20;
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SbRestAppApplication.class, args);
+    }
 }
